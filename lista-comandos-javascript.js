622064ee051d8b5fcf3c242b9a4bf7dea1dfc770
FERRAZG <ferrazg423@gmail.com>
2021-09-10 14:52:20 -0300

    lista de comandos da aula de Logica de Programaçao

---
+++ lista-comandos-javascript.js
@@ -0,0 +1,60 @@
+//variavel pode sser alterada durante o Programa LET
+//let nomeEstudante = "Guilherme";
+//console.log (nomeEstudante);
+//variavel nao sofre alteração 
+//const nomeDoestudante = "Julia" ;
+// console.log (nomeDoestudante)
+
+//Criação de Lista 
+//let listaDeEstudantes = ["Guilherme", "Julia", "Laura"];
+//let quantidadeDeEestudantes = listaDeEstudantes.length;
+//console.log (quantidadeDeEestudantes);
+
+// comando For possibilita percorer a partir do indice 0 ate seu total
+//let listaDeEstudantes = ["Guilherme", "Julia", "Laura"];
+//let quantidadeDeEestudantes = listaDeEstudantes.length;
+
+  //  for(let indice = 0; indice < quantidadeDeEestudantes; indice++) {
+    //    const alunoCorrente = listaDeEstudantes[indice];
+      //  console.log(alunoCorrente);    }
+
+//estruturas de repeticoes e acesso a lista 
+//let listaDeEstudantes = ["Guilherme", "Julia", "Laura"];
+//let quantidadeDeEestudantes = listaDeEstudantes.length;
+//let inicio = 0;
+//Rotina com DO
+    //do{
+      //  console.log(listaDeEstudantes[inicio]) ;
+      //  inicio++ ;
+  //}while (inicio < quantidadeDeEestudantes);
+
+  //Rotina com WHILE
+      //while (inicio < quantidadeDeEestudantes) {
+        //console.log(listaDeEstudantes[]inicio);
+        //inicio++ ;      }
+ 
+//Estrutura de Contagem 
+        //for (let numero = 1 ; numero <= 10 ; numero++ ) {
+   //console.log(numero); }
+
+   //Adicionando um dado a variavel 
+//listaDeEstudantes.push("Andrea");
+//console.log(listaDeEstudantes);
+
+//adicionar um novo item a lista
+//let listaDeEstudantes = ["Guilherme", "Julia", "Laura", "Andrea"];
+//let quantidadeDeEestudantes = listaDeEstudantes.length;
+  //if (quantidadeDeEestudantes < 5) {
+  //listaDeEstudantes.push("Andrea"); }
+//console.log(listaDeEstudantes);
+
+//Quando limitar quantidades de itens a serem adicionados 
+//let listaDeEstudantes = ["Guilherme", "Julia", "Laura", "Andrea"];
+//let quantidadeDeEestudantes = listaDeEstudantes.length;
+  //if (quantidadeDeEestudantes < 4) {
+    //listaDeEstudantes.push("Moises"); 
+    //console.log(listaDeEstudantes);
+  //} else {
+    //  console.log("Não é possivel adicionar mais alunos nessa turma ")  }
+  
+
